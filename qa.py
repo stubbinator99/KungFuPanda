319147fb04ddbdc7f2704ddbed2b6eeb652c0528
Jonathan <jonathan.call@outlook.com>
2018-10-25 15:23:03 -0600

    Debugged and refactored our existing code, now it reads in the files correctly

--- qa.py
+++ qa.py
@@ -6,41 +6,56 @@ import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 import sys
 
-input = sys.argv[1]
+#input = sys.argv[1]
+input = "input.txt"
 filePath = ""
 storyIdList = []
 
 with open(input, "r") as inputFile:
-  filePath = inputFile.readline()
-  line = inputFile.readline()
-  while line:
-    storyIdList.append(inputFile.readline())
+  filePath = inputFile.readline().strip()
+  for line in inputFile:
+    storyIdList.append(line.strip())
 
 storyText = ""
 storySens = []
 for storyDir in storyIdList:
-  with open(filePath + storyDir + ".story") as storyFile:
-    line = storyFile.readline() # Headline
-    line = storyFile.readline() # Date
-    line = storyFile.readline() # StoryID
+  # Read in the story file
+  storyFilePath = filePath + storyDir + ".story"
+  print(storyFilePath)
+
+  with open(storyFilePath) as storyFile:
+    line = storyFile.readline().strip()  # Date
+    line = storyFile.readline().strip()  # Headline
+    line = storyFile.readline().strip()  # StoryID
     storyID = line[1]
-    line = storyFile.readline() # Blank line
-    line = storyFile.readline() # TEXT:
+    line = storyFile.readline().strip()  # Blank line
+    line = storyFile.readline().strip()  # TEXT:
     line = storyFile.readline()  # Blank line
-    while line:
-      line = storyFile.readLine()
-      storyText += line
+    for line in storyFile:
+      storyText += line.strip() + " "
     storySens = sent_tokenize(storyText)
 
+    # Read in the question file
   with open(filePath + storyDir + ".questions") as questionFile:
-    while line:
-      line = questionFile.readline()
-      questionId = line[1]
-      line = questionFile.readline()
-      question = line[1:]
-      line = questionFile.readLine() # The difficulty
-      #difficulty = line[1]
-      line = questionFile.readLine() # The blank line
+    count = 0
+    questionId = ""
+    question = []
+    difficulty = ""
+    for line in questionFile:
+        line = line.split()
+        if count == 0:
+            questionId = line[1]
+            count = 1
+        elif count == 1:
+            question = line[1:]
+            count = 2
+        elif count == 2:
+            difficulty = line[1]
+            count = 3
+        elif count == 3:
+            # Question block complete. Answer the question and output it
 
-      #find answer to the question and output it
+            # Set count, questionId, question, and difficulty to default values before processing the next set question
+            count = 0
 
+print("Done!")
