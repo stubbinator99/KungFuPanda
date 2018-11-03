900da2a2e3db88798851a89e8bd73ece882ebdca
Eric <stubbsguitar@gmail.com>
2018-11-03 17:50:43 -0600

    added more spacy stuff, currently won't answer questions though. look for the TODO

--- qa.py
+++ qa.py
@@ -9,7 +9,7 @@ import spacy
 from nltk.chunk import conlltags2tree
 import sys
 
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_md')
 
 input_file = sys.argv[1]    # Index file to read in
 data_directory_path = ""    # Path to the directory containing the data
@@ -61,6 +61,7 @@ for storyId in storyIdList:
   chunked_sens = []
   ner_sens = []
   nltk_format_ner_sens = []
+  sen_index = 0
 
   for sen in storySents:
     #tokenized_sen = word_tokenize(sen)
@@ -79,13 +80,13 @@ for storyId in storyIdList:
     chunked_sens.append(sen.noun_chunks)
 
     tagged_sens.append([])
-    #ner_sens.append([])
+    ner_sens.append([])
     for token in sen:
       tagged_sens[len(tagged_sens)-1].append([token.text, token.tag_])
 
 
       # Sentences in spacy ner format
-      ner_sens.append([#[len(ner_sens)-1].append([
+      ner_sens[sen_index].append([#[len(ner_sens)-1].append([
           token.text,
           token.tag_,
           "{0}-{1}".format(token.ent_iob_, token.ent_type_) if token.ent_iob_ != 'O' else token.ent_iob_
@@ -94,6 +95,8 @@ for storyId in storyIdList:
       #nltk_format_ner_sens.append(conlltags2tree(ner_sens[len(nltk_format_ner_sens)]))
 
     #tagged_sen = pos_tag(tokenized_sen)
+
+    sen_index += 1
 
   # Read in the question file to: ---------------------------------------------------------------------------
   #   1.
@@ -124,34 +127,71 @@ for storyId in storyIdList:
         total_questions = total_questions + 1
 
         # Run NER on the question
-        tagged = nltk.pos_tag(question_word_list)
-        named_ent = nltk.ne_chunk(tagged)
+        #tagged = nltk.pos_tag(question_word_list)
+        #named_ent = nltk.ne_chunk(tagged)
 
-        for element in named_ent:
-          elementStr = str(element)
-          if elementStr[1] != "'" and elementStr[1] != "\"":
-            entity = str(element).split()[0][1:]
+        for sen in doc.sents:
+          print(sen)
+
+        spacy_q = nlp(question)
+
+        for sen in doc.sents:
+          print(sen)
+
+        tagged_q = []
+        ner_q = []
+        for token in spacy_q:
+          tagged_q.append([token.text, token.tag])
+          ner_q.append([
+            token.text,
+            token.tag_,
+            "{0}-{1}".format(token.ent_iob_, token.ent_type_) if token.ent_iob_ != 'O' else token.ent_iob_
+          ])
+
+        # for element in named_ent:
+        #   elementStr = str(element)
+        #   if elementStr[1] != "'" and elementStr[1] != "\"":
+        #     entity = str(element).split()[0][1:]
 
         #print(question)
         question_entity_types = []
-        # Entity tags (from ace): ['LOCATION', 'ORGANIZATION', 'PERSON', 'DURATION',
+        # NLTK Entity tags (from ace): ['LOCATION', 'ORGANIZATION', 'PERSON', 'DURATION',
         #             'DATE', 'CARDINAL', 'PERCENT', 'MONEY', 'MEASURE', 'FACILITY', 'GPE']
 
+        # Spacy Entity tags: ['PERSON', 'NORP', 'FAC', 'ORG', 'GPE', 'LOC', 'PRODUCT', 'EVENT', 'WORK_OF_ART',
+        #             'LAW', 'LANGUAGE', 'DATE', 'TIME', 'PERCENT', 'MONEY', 'QUANTITY', 'ORDINAL', 'CARDINAL']
+
         if "Who" in question_word_list or "who" in question_word_list or "Whose" in question_word_list or "whose" in question_word_list:
+          # nltk entity types
+          #question_entity_types.append("PERSON")
+          #question_entity_types.append("ORGANIZATION")
+          #question_entity_types.append("GPE")
+
+          # spacy entity types
           question_entity_types.append("PERSON")
-          question_entity_types.append("ORGANIZATION")
+          question_entity_types.append("NORP")
+          question_entity_types.append("ORG")
           question_entity_types.append("GPE")
         elif "What" in question_word_list or "what" in question_word_list:
           # Other notes: 'at what point' (like a 'when' question), 'what type', 'what happened', 'what x' (what book, what organization, etc.)
           question_entity_types = []
         elif "When" in question_word_list or "when" in question_word_list:
+          #question_entity_types.append("DATE")
+          #question_entity_types.append("TIME")
+
           question_entity_types.append("DATE")
           question_entity_types.append("TIME")
+          #question_entity_types.append("ORDINAL") #first, second, etc.
         elif "Where" in question_word_list or "where" in question_word_list:
           # Other notes: 'where in x' (where in Canada, etc)
-          question_entity_types.append("LOCATION")
-          question_entity_types.append("FACILITY")
+          #question_entity_types.append("LOCATION")
+          #question_entity_types.append("FACILITY")
+          #question_entity_types.append("GPE")
+
+          question_entity_types.append("FAC")
+          #question_entity_types.append("ORG")
           question_entity_types.append("GPE")
+          question_entity_types.append("LOC")
         elif "How" in question_word_list or "how" in question_word_list:
           # Other notes: 'how long', 'how many', 'how much', 'how often', 'how far', 'by how much'
           question_entity_types = []
@@ -161,7 +201,7 @@ for storyId in storyIdList:
 
         # Do NER for each sentence in the story
         question_answered = False
-        for sen in storySents:
+        for sen in doc.sents:
           #tokenized_sen = word_tokenize(sen)
           #tagged_sen = pos_tag(tokenized_sen)
 
@@ -171,11 +211,16 @@ for storyId in storyIdList:
           unique_sentence_entities = set()
           #named_ent_sentence = nltk.ne_chunk(tagged_sen)
           named_ent_sentence = []
-          for element in named_ent_sentence:
-            elementStr = str(element)
-            if elementStr[1] != "'" and elementStr[1] != "\"":
-              entity = str(element).split()[0][1:]
-              unique_sentence_entities.add(entity)
+
+          #for element in named_ent_sentence:
+          #  elementStr = str(element)
+          #  if elementStr[1] != "'" and elementStr[1] != "\"":
+          #    entity = str(element).split()[0][1:]
+          #    unique_sentence_entities.add(entity)
+
+          for i in range(len(ner_sens)):
+            elementStr = str(ner_sens[i])
+            #TODO: add code here to pull out entities, or change spacy ner tagging format
 
           for entity in question_entity_types:
             if entity in unique_sentence_entities:
