87239a2e11c79f1296e6a501b3f65ca484898510
Jonathan <jonathan.call@outlook.com>
2018-11-06 16:41:06 -0700

    Fiexd the answer key generator

--- make_answer_key.py
+++ make_answer_key.py
@@ -12,14 +12,15 @@ with open(input_file, "r") as inputFile:
 # Process each story and its associated questions and answers
 for storyId in storyIdList:
   answerFilePath = data_directory_path + storyId + ".answers"
-  with open(answerFilePath) as storyFile:
-    for line in answerFilePath:
-      if line.split()[0] == "QuestionID:":
-        print(line.strip())
-      elif line.split()[0] == "Question:":
-        print(line.strip())
-      elif line.split()[0] == "Answer:":
-        print(line.strip())
-      elif line.split()[0] == "Difficulty:":
-        print(line.strip())
-        print()
+  with open(answerFilePath) as answerFile:
+    for line in answerFile:
+      if len(line.split()) > 0:
+        if line.split()[0] == "QuestionID:":
+          print(line.strip())
+        elif line.split()[0] == "Question:":
+          print(line.strip())
+        elif line.split()[0] == "Answer:":
+          print(line.strip())
+        elif line.split()[0] == "Difficulty:":
+          print(line.strip())
+          print()
